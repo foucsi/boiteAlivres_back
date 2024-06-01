d2cb58aced4a0a978c8a0c80f47190ca57276afc
Foucsi <julien.foucartreact@gmail.com>
2024-06-01 09:45:10 +0200

    add model bookPlaces

--- models/bookPlaces.js
+++ models/bookPlaces.js
@@ -12,6 +12,12 @@ const AddressSchema = new mongoose.Schema(
 
 const BookPlaceSchema = new mongoose.Schema({
     addedBy: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+    latitude: { type: Number, required: true},
+    longitude: { type: Number, required: true },
+    description: { type: String, required: true },
+    date_added: { type: Date, default: Date.now },
+    photo:{ type: String, default: "https://via.placeholder.com/150"},
+    icon: { type: String, default: "https://via.placeholder.com/150"},
     adress: AddressSchema
 })
 module.exports = mongoose.model("bookPlaces", BookPlaceSchema);
