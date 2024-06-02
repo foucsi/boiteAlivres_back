7bbc57bbb2468d6da7ed5eb4e665b73307d60701
Foucsi <julien.foucartreact@gmail.com>
2024-06-02 21:17:07 +0200

    add middleware bokkPalce not found

--- routes/bookPlaces.js
+++ routes/bookPlaces.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 const bookPlaces_post_controllers = require("../controllers/BOOKPLACES/bookPlaces_post_controllers");
 const bookPlaces_delete_controllers = require("../controllers/BOOKPLACES/bookPlaces_delete_controllers");
+const bookPlaces_get_controllers = require("../controllers/BOOKPLACES/bookPlaces_get_controllers");
+
 //GET
+router.get("/getBookPlaces", bookPlaces_get_controllers.getBookPlaces);
 
 //POST
 router.post("/addBookPlace/:uniqueId", bookPlaces_post_controllers.addBookPlace);
