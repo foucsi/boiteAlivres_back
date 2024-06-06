3e63692a19464d04d2ce4db44df341a7e7976dad
Foucsi <julien.foucartreact@gmail.com>
2024-06-06 19:38:33 +0200

    add rte comments

---
+++ controllers/COMMENTS/comments_post_controllers.js
@@ -0,0 +1,12 @@
+const User = require("../../models/users");
+const BookPlace = require("../../models/bookPlaces");
+const Comment = require("../../models/comments");
+
+exports.addComments = async (req, res, next) => {
+    try{
+
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const comments_get_controllers = require("../controllers/COMMENTS/comments_get_controllers");
+const comments_post_controllers = require("../controllers/COMMENTS/comments_post_controllers");
+
 //GET
 
 //POST
-
+router.post("/addComment/:uniqueId", comments_post_controllers.addComment);
 //DELETE
 
 //UPDATE
