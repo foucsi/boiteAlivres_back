cdb0f946fd521e1faa68d92eaf929d5894fa9076
Foucsi <julien.foucartreact@gmail.com>
2024-06-06 19:46:24 +0200

    add rte comments

--- controllers/COMMENTS/comments_get_controllers.js
+++ controllers/COMMENTS/comments_get_controllers.js
@@ -2,7 +2,12 @@ const Comment = require("../../models/comments");
 
 exports.getAllComments = async (req, res, next) => {
     try{
-
+        const comments = await Comment.find().populate('added_by');
+        if(!comments) {
+            const err = new Error("Not comments in database");
+            return next(err)
+        }
+        return res.status(200).json({result: true, comments:comments})
     }catch(err){
         console.error(err)
         next(err)
