53fbcf3645b6bbea07cca635eef90a982dc80336
Foucsi <julien.foucartreact@gmail.com>
2024-06-09 08:53:50 +0200

    add reducer comments

--- controllers/BOOKPLACES/bookPlaces_update_controllers.js
+++ controllers/BOOKPLACES/bookPlaces_update_controllers.js
@@ -16,3 +16,22 @@ exports.updateBookPlace = async (req, res, next) => {
             next(err)
     }
 }
+
+//update description of bookPlace
+
+exports.updateDescription = async (req, res, next) => {
+    const {bookPlaceId}=req.params;
+    const {description}=req.body;
+    try{
+        const bookPlace = await BookPlaces.findByIdAndUpdate(bookPlaceId, {description: description}, {new: true});
+        if(!bookPlace) {
+            const error = new Error("BookPlace not found");
+            return next(error);
+        }
+        return res.status(200).json({result:true, message: "BookPlace updated successfully", bookPlace: bookPlace});
+    }catch(err){
+        console.error(err);
+        next(err)
+    }
+
+}
