75d73a945a6ea85b4aad6d7a136ad4501667964f
Foucsi <julien.foucartreact@gmail.com>
2024-06-19 14:40:13 +0200

    add rte usersPremium

--- controllers/USERS/users_get_controllers.js
+++ controllers/USERS/users_get_controllers.js
@@ -10,7 +10,8 @@ exports.getAllUsers = async (req, res, next) => {
             const err = new Error("Not users in database");
             return next(err);
         }
-        return res.json({ result: true, users: users });
+        const usersPremium = users.filter(user => user.premium === true);
+        return res.json({ result: true, users: users, usersPremium: usersPremium });
     } catch (err) {
         console.error(err.message);
         next(err);
