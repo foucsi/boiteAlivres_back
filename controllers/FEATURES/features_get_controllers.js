b1e1eaed79347563de88e50c74e1ebbb2e5af6ae
Foucsi <julien.foucartreact@gmail.com>
2024-06-24 16:15:47 +0200

    getAllFeatures

--- controllers/FEATURES/features_get_controllers.js
+++ controllers/FEATURES/features_get_controllers.js
@@ -7,7 +7,7 @@ exports.getAllFeatures = async (req, res, next) => {
             const error = new Error("Not features in database")
             return next(error)
         }
-        res.status(200).json({success: true, features: features})
+        res.status(200).json({result: true, features: features})
     }catch(err){
         console.error(err)
         next(err)
