d9f19b01f0ac6bee2848ff430584bc1ff38cee32
Foucsi <julien.foucartreact@gmail.com>
2024-06-28 09:33:32 +0200

    add rte delete favorite

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const favorites_post_controllers = require("../controllers/FAVORITES/favorites_post_controllers");
 const favorites_get_controllers = require("../controllers/FAVORITES/favorites_get_controllers");
+const favorites_delete_controllers = require("../controllers/FAVORITES/favorites_delete_controllers");
 
 //GET
 router.get("/allFavoritesByUser/:uniqueId/:bookPlaceId", favorites_get_controllers.getFavoritesByUserId);
@@ -12,7 +13,7 @@ router.get("/favoritesByUser/:uniqueId", favorites_get_controllers.getFavorites)
 router.post("/addFavorite/:uniqueId/:bookPlaceId", favorites_post_controllers.addFavorite);
 
 //DELETE
-
+router.post("/deleteFavorite/:favoriteId", favorites_delete_controllers.deleteFavorite);
 //UPDATE
 
 module.exports = router;
