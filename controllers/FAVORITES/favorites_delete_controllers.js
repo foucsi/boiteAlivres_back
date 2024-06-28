f357feb32d4779fb58338e6a37bda3dbbcbb3258
Foucsi <julien.foucartreact@gmail.com>
2024-06-28 09:30:54 +0200

    add rte delete favorite

--- controllers/FAVORITES/favorites_delete_controllers.js
+++ controllers/FAVORITES/favorites_delete_controllers.js
@@ -1,7 +1,9 @@
+const Favorite = require("../../models/favorites")
+
 exports.deleteFavorite = async(req, res, next) => {
     const {favoriteId} = req.params
     try{
-
+        const favorite = await Favorite.findById({_id: favoriteId})
     }catch(err){
         console.error(err)
         next(err)
