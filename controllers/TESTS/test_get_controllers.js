57556bc537633303bc3395dc502d4f5fc941205e
Foucsi <julien.foucartreact@gmail.com>
2024-07-03 18:57:33 +0200

    add test routes

---
+++ controllers/TESTS/test_get_controllers.js
@@ -0,0 +1,4 @@
+exports.testRoute = (req, res, next) => {
+
+    const {number} = req.body
+}
--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const test_get_controllers = require("../controllers/TESTS/test_get_controllers");
+
 //GET
+router.get("/testRoutes", test_get_controllers.testRoutes);
 
 module.exports = router;
