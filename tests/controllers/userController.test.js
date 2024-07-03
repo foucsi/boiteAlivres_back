f4b4400c124a529b600f80e2c91bc34bb3a7e12d
Foucsi <julien.foucartreact@gmail.com>
2024-07-03 14:44:38 +0200

    add jets test

--- tests/controllers/userController.test.js
+++ tests/controllers/userController.test.js
@@ -1,2 +1,29 @@
-const {getAllUsers} = require("../../controllers/USERS/users_get_controllers")
-const User = require("../../db/models/users")
+const getAllUsers = require('../../controllers/USERS/users_get_controllers').getAllUsers;
+const User = require('../../db/models/users');
+
+jest.mock('../../db/models/users');
+
+describe('getAllUsers', () => {
+    it('should return all users', async () => {
+        const mockUsers = [
+            { name: 'User1', premium: true },
+            { name: 'User2', premium: false }
+        ];
+
+        User.find.mockResolvedValue(mockUsers);
+
+        const mockReq = {};
+        const mockRes = {
+            json: jest.fn()
+        };
+        const mockNext = jest.fn();
+
+        await getAllUsers(mockReq, mockRes, mockNext);
+
+        expect(mockRes.json).toHaveBeenCalledWith({
+            result: true,
+            users: mockUsers,
+            usersPremium: [mockUsers[0]]
+        });
+    });
+});
