8ea0fee178cde29d3b21ded087d2a9363429b695
Foucsi <julien.foucartreact@gmail.com>
2024-07-12 16:18:10 +0200

    add timestamps in mongoose schema

--- db/models/users.js
+++ db/models/users.js
@@ -6,13 +6,10 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true, minLength: 6 },
-    // add created_at
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
     uniqueId: { type: String }, //token uuid4
     photo: { type: String, default: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png"},
     premium: { type: Boolean, default: true},
     badge: { type: String, default: "premium", required: function() { return this.premium === true;}}
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model("users", UserSchema);
