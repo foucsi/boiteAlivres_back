c7310bcb3cb31fa314aae34af5da8ea2b039fd0f
Foucsi <julien.foucartreact@gmail.com>
2024-07-24 10:02:14 +0200

    add middlawre favorites not found

--- middlewares/favoritesNotFound.js
+++ middlewares/favoritesNotFound.js
@@ -1,6 +1,6 @@
 module.exports = function favoritesNotFound(err, req, res, next) {
     if (err.message === "Favorite not found") {
-        return res.status(200).json({result: false, message: "Favorite not found"})
+        return res.status(200).json({result: false, error: "Favorite not found"})
     }else{
         next(err)
     }
