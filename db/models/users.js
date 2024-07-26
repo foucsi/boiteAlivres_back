6e7e48df6700f9b45111c630acd6f2b2e105dc46
Foucsi <julien.foucartreact@gmail.com>
2024-07-26 09:02:18 +0200

    add share model user

--- db/models/users.js
+++ db/models/users.js
@@ -10,7 +10,26 @@ const UserSchema = new mongoose.Schema({
     photo: { type: String, default: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png"},
     premium: { type: Boolean, default: true},
     badge: { type: String, default: "premium", required: function() { return this.premium === true;}},
-    share:{type: String, enum:["debutant", "lecteur regulier", "bibliophile", "Maître des livres", "Grand sage de la littérature"], default: "debutant"},
+    share: {
+        level: {
+            type: Number,
+            enum: [1, 2, 3, 4, 5],
+            default: 1
+        },
+        badge: {
+            type: String,
+            enum: ["debutant", "lecteur regulier", "bibliophile", "Maître des livres", "Grand sage de la littérature"],
+            default: "debutant"
+        },
+        totalShared: {
+            type: Number,
+            default: 0
+        },
+        lastUpdated: {
+            type: Date,
+            default: Date.now
+        }
+    }
 }, {timestamps: true});
 
 module.exports = mongoose.model("users", UserSchema);
