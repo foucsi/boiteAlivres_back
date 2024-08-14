a299b32b7db9d49f413210f88761f6fa50b35d2a
Foucsi <julien.foucartreact@gmail.com>
2024-08-14 10:09:21 +0200

    add nodemailer coté back

--- controllers/NODEMAILER/nodemailer_post_controllers.js
+++ controllers/NODEMAILER/nodemailer_post_controllers.js
@@ -1,5 +1,6 @@
 const Msg = require("../../db/models/signalMsgs")
 const nodemailer = require('nodemailer')
+const User = require("../../db/models/users")
 
 exports.sendMsg = async(req, res, next) => {
     const {message,email} = req.body
@@ -7,5 +8,28 @@ exports.sendMsg = async(req, res, next) => {
     if(!message || !email){
         const error = new Error("Message or email is missing")
         return next(error)
+    }
+
+    try{
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: process.env.USER_PASS,
+                pass: process.env.PASWWORD_USER_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: email,
+            to: process.env.EMAIL_TEST, // L'adresse où vous recevez les signalements
+            subject: 'Nouveau signalement',
+            text: message,
+        };
+
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('Signalement envoyé avec succès.');
+    }catch (err){
+        console.error(err)
+        next(err)
     }
 }
