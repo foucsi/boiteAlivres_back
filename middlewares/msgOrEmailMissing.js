1f515d2bc4baa462e375e6eacb5411af8db11749
Foucsi <julien.foucartreact@gmail.com>
2024-08-14 15:14:50 +0200

    custom modal send signal msg

--- middlewares/msgOrEmailMissing.js
+++ middlewares/msgOrEmailMissing.js
@@ -1,6 +1,6 @@
 module.exports = function msgOrEmailMissing(err, req, res, next) {
     if (err.message === "Message is missing") {
-        return res.status(200).json({result: false, error: "Message is missing"})
+        return res.status(200).json({result: false, error: "Message vide !"})
     }else{
         next(err)
     }
