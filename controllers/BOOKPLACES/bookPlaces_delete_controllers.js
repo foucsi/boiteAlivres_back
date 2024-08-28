f092427d8f27946804c23eaf6f77917619f41336
Foucsi <julien.foucartreact@gmail.com>
2024-08-28 11:15:16 +0200

    add asyncHandler on controllers - refacto code review

--- controllers/BOOKPLACES/bookPlaces_delete_controllers.js
+++ controllers/BOOKPLACES/bookPlaces_delete_controllers.js
@@ -1,17 +1,13 @@
 const BookPlace = require("../../db/models/bookPlaces");
+const asyncHandler = require("express-async-handler");
 
 //Delete bookplaces
-exports.deleteBookPlace = async(req, res, next) => {
+exports.deleteBookPlace =asyncHandler( async(req, res, next) => {
     const {id} = req.params
-    try{
         const bookPlace = await BookPlace.findByIdAndDelete({_id:id})
         if(!bookPlace){
             const err = new Error("BookPlace not found")
             return next(err)
         }
         return res.json({result:true, message:"BookPlace deleted"})
-    }catch (err){
-        console.error(err)
-        next(err)
-    }
-}
+})
