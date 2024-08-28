2637dfc5bb24bdbad043518f0ca32a7def1196c1
Foucsi <julien.foucartreact@gmail.com>
2024-08-28 11:20:11 +0200

    add asyncHandler on controllers - refacto code review

--- controllers/FAVORITES/favorites_delete_controllers.js
+++ controllers/FAVORITES/favorites_delete_controllers.js
@@ -1,16 +1,12 @@
 const Favorite = require("../../db/models/favorites")
+const asyncHandler = require("express-async-handler")
 
-exports.deleteFavorite = async(req, res, next) => {
+exports.deleteFavorite = asyncHandler( async(req, res, next) => {
     const {favoriteId} = req.params
-    try{
         const favorite = await Favorite.findByIdAndDelete({_id: favoriteId})
         if(!favorite){
             const err = new Error("Favorite not found")
             return next(err)
         }
         return res.status(200).json({result: true})
-    }catch(err){
-        console.error(err)
-        next(err)
-    }
-}
+})
