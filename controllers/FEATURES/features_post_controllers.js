7ba41266c3e039191a8b9148dea76d9f3a7ab238
Foucsi <julien.foucartreact@gmail.com>
2024-08-29 18:41:00 +0200

    add asyncHandler on controllers - refacto code review

--- controllers/FEATURES/features_post_controllers.js
+++ controllers/FEATURES/features_post_controllers.js
@@ -1,16 +1,12 @@
 const Feature = require("../../db/models/features")
+const asyncHandler = require("express-async-handler");
 
-exports.addFeature = async (req, res, next) => {
+exports.addFeature = asyncHandler( async (req, res, next) => {
     const {feature, description} = req.body
-    try{
         const newFeature = new Feature({
             feature,
             description
         })
         await newFeature.save();
         return res.status(200).json({result: true, feature: newFeature})
-    }catch(err){
-        console.error(err)
-        next(err)
-    }
-}
+})
