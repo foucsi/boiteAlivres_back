04386bdf52385d7abfa0d9b6ece7e819231e3e41
Foucsi <julien.foucartreact@gmail.com>
2024-09-05 10:43:39 +0200

    add timestamp model favorites

--- db/models/favorites.js
+++ db/models/favorites.js
@@ -4,6 +4,6 @@ const FavoriteSchema = new mongoose.Schema({
     bookPlace: {type: mongoose.Schema.Types.ObjectId, ref: 'bookPlaces'},
     user:{type: mongoose.Schema.Types.ObjectId, ref: 'users'},
     date_added: {type: Date, default: Date.now}
-})
+}, { timestamps: true })
 
 module.exports = mongoose.model('favorites', FavoriteSchema)
