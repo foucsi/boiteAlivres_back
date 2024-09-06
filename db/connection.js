846a36e7dd1fbd36b77deb01c8339b210bce7b17
Foucsi <julien.foucartreact@gmail.com>
2024-09-06 10:46:42 +0200

    add asynchandler connect db

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-const asyncHandler = require("express-async-handler");
-
 const connectionString = process.env.CONNECTION_STRING;
 
-const connectDb = asyncHandler( async ()=>{
+const connectDb = async ()=>{
+    try{
         await mongoose.connect(connectionString, {connectTimeoutMS: 2000});
         console.log("Database app boiteAlivres connected");
-})
+    }catch(error){
+        console.error(error);
+    }
+}
 
 connectDb()
