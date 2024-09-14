8f8ccc188c3712d7a47e936d6c3f3b558b3bddce
Foucsi <julien.foucartreact@gmail.com>
2024-09-14 09:32:10 +0200

    add architecture modulaire

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const comments_get_controllers = require("../controllers/COMMENTS/comments_get_controllers");
-const comments_post_controllers = require("../controllers/COMMENTS/comments_post_controllers");
-const comments_delete_controllers = require("../controllers/COMMENTS/comments_delete_controllers");
+const comments_get_controllers = require("../comments/controllers/comments_get_controllers");
+const comments_post_controllers = require("../comments/controllers/comments_post_controllers");
+const comments_delete_controllers = require("../comments/controllers/comments_delete_controllers");
 //GET
 router.get("/getAllComments", comments_get_controllers.getAllComments);
 router.get("/getAllCommentsByBookPlace/:bookPlaceId", comments_get_controllers.getAllCommentsByBookPlace);
