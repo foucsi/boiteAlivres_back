1d57420234eefc399f2d7942f8fbc479b905a297
Foucsi <julien.foucartreact@gmail.com>
2024-09-23 10:04:37 +0200

    update sendResponse

--- utils/sendReponse.js
+++ utils/sendReponse.js
@@ -22,7 +22,7 @@ exports.sendResponse = (res, statusCode, data, message) => {
     }
     res.status(statusCode).json({
         success: statusCode >= 200 && statusCode < 300,
-        message: message,
+        message: message.trim(),
         ...data
     });
 }
