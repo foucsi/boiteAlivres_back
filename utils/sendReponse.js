f13f5ed2d5eb3d798febb63dbc478a5e1cc67e88
Foucsi <julien.foucartreact@gmail.com>
2024-09-23 10:03:52 +0200

    add @params sendResponse

--- utils/sendReponse.js
+++ utils/sendReponse.js
@@ -1,6 +1,25 @@
 // utils/sendResponse.js
 
+/**
+ * Envoie une réponse JSON standardisée.
+ * @param {Object} res - L'objet de réponse Express.
+ * @param {number} statusCode - Le code de statut HTTP.
+ * @param {Object} [data={}] - Les données à inclure dans la réponse.
+ * @param {string} [message=''] - Un message optionnel à inclure dans la réponse.
+ */
+
 exports.sendResponse = (res, statusCode, data, message) => {
+    if (typeof statusCode !== 'number') {
+        throw new Error('statusCode doit être un nombre');
+    }
+
+    if (typeof message !== 'string') {
+        throw new Error('message doit être une chaîne de caractères');
+    }
+
+    if (typeof data !== 'object' || data === null) {
+        data = {};
+    }
     res.status(statusCode).json({
         success: statusCode >= 200 && statusCode < 300,
         message: message,
