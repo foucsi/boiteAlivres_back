cafedf161471f90da0c1a9decf5a3903a04a576b
Foucsi <julien.foucartreact@gmail.com>
2024-09-26 17:06:25 +0200

    add sendResponse

--- controllers/FEATURES/features_get_controllers.js
+++ controllers/FEATURES/features_get_controllers.js
@@ -1,6 +1,8 @@
 const Feature = require("../../db/models/features")
 const asyncHandler = require("express-async-handler");
 
+const {sendResponse} = require("../../utils/sendReponse")
+
 //get all features
 exports.getAllFeatures = asyncHandler( async (req, res, next) => {
         const features = await Feature.find()
@@ -8,5 +10,6 @@ exports.getAllFeatures = asyncHandler( async (req, res, next) => {
             const error = new Error("Not features in database")
             return next(error)
         }
+        return sendResponse(res, 200, {features}, "Features found")
         res.status(200).json({result: true, features: features})
 })
