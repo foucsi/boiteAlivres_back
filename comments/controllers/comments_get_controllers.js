5da79715f8466aa89bf373eb0e7ad72094463c28
Foucsi <julien.foucartreact@gmail.com>
2024-09-27 11:44:40 +0200

    add error response files

--- comments/controllers/comments_get_controllers.js
+++ comments/controllers/comments_get_controllers.js
@@ -12,7 +12,6 @@ exports.getAllComments = asyncHandler( async (req, res, next) => {
             return errorResponse(res, 404, "No comments in database")
         }
         return sendResponse(res,200,{comments}, "All comments")
-        // return res.status(200).json({result: true, comments:comments})
 })
 
 //get all comments by bookPlaceId
@@ -20,15 +19,12 @@ exports.getAllCommentsByBookPlace = asyncHandler( async (req, res, next) => {
     const {bookPlaceId} = req.params;
         const bookPlace = await BookPlace.findById({_id: bookPlaceId});
         if(!bookPlace){
-            const err = new Error("BookPlace not found");
-            return next(err)
+            return errorResponse(res, 404, "No bookPlace with this id")
         }
         const comments = await Comment.find({bookPlaceId
                 : bookPlaceId}).populate('added_by');
         if(!comments){
-            const err = new Error("Not comments in database");
-            return next(err)
+            return errorResponse(res, 404, "No comments in database")
         }
         return sendResponse(res,200,{comments}, "All comments by bookPlace")
-        // return res.status(200).json({result: true, comments:comments})
 })
