c70387db5dc79d9c377a2c0cc02dc00d5b5e7e0a
Foucsi <julien.foucartreact@gmail.com>
2024-09-27 11:47:15 +0200

    add error response files

--- controllers/FAVORITES/favorites_get_controllers.js
+++ controllers/FAVORITES/favorites_get_controllers.js
@@ -4,33 +4,6 @@ const BookPlace = require("../../db/models/bookPlaces")
 const asyncHandler = require("express-async-handler")
 
 const {sendResponse} = require("../../utils/sendReponse")
-//
-// exports.getFavoritesByUserId = async (req, res, next) => {
-//     const {uniqueId, bookPlaceId} = req.params
-//     try {
-//         const [existingUser, existingBookPlace] = await Promise.all([
-//             User.findOne({uniqueId}),
-//             BookPlace.findById(bookPlaceId)
-//         ])
-//         if (!existingUser) {
-//             return res.status(404).json({result: false, message: "User not found"})
-//         }
-//         if (!existingBookPlace) {
-//             return res.status(404).json({result: false, message: "BookPlace not found"})
-//         }
-//         const existingFavorite = await Favorite.findOne({
-//             bookPlace: existingBookPlace._id,
-//             user: existingUser._id
-//         })
-//         if (!existingFavorite) {
-//             return res.status(404).json({result: false, message: "Favorite not found"})
-//         }
-//         return res.status(200).json({result: true, favorite: existingFavorite})
-//     } catch(err) {
-//         console.error(err)
-//         next(err)
-//     }
-// }
 
 exports.getFavoritesByUserId = asyncHandler( async (req, res, next) => {
     const {uniqueId, bookPlaceId} = req.params
