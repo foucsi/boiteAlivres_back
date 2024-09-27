a884b71c8eb44882ead55c8c029300c898f2f7bc
Foucsi <julien.foucartreact@gmail.com>
2024-09-27 11:53:16 +0200

    add error response files

--- users/controllers/users_get_controllers.js
+++ users/controllers/users_get_controllers.js
@@ -1,16 +1,19 @@
 const User = require("../../db/models/users")
 const asyncHandler = require("express-async-handler")
+
+//utils send response and errorResponse
 const {sendResponse}= require("../../utils/sendReponse")
+const {errorResponses}= require("../../utils/errorResponses")
+
+
 //GET ALL USERS
 exports.getAllUsers = asyncHandler(async (req, res, next) => {
         const users = await User.find().select("-password -__v").lean();
         if (!users || users.length === 0) {
-            const err = new Error("Not users in database");
-            return next(err);
+            return errorResponses(res, 404, "Not users in database");
         }
         const usersPremium = users.filter(user => user.premium === true);
-        // return res.json({ result: true, users: users, usersPremium: usersPremium });
-    return sendResponse(res, 200, { users, usersPremium }, "All users");
+        return sendResponse(res, 200, { users, usersPremium }, "All users");
 });
 
 
@@ -19,9 +22,7 @@ exports.infoByUser = asyncHandler(async (req, res, next) => {
     const {id} = req.params;
     const user = await User.findById(id).select("-password -__v").lean();
     if (!user || user.length === 0) {
-        const err = new Error("Not users in database");
-        return next(err);
+        return errorResponses(res, 404, "User not found");
     }
-    // return res.json({ result: true, user: user });
     return sendResponse(res, 200, { user }, "User info");
 })
